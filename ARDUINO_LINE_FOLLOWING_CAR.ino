fcce3deb393085a22c2d55610215a8030a487039
Hasan Alshikh <52888308+hsndev18@users.noreply.github.com>
2020-04-27 17:24:30 +0300

    Add files via upload

---
+++ ARDUINO_LINE_FOLLOWING_CAR.ino
@@ -0,0 +1,66 @@
+#include <AFMotor.h>
+#define left A0
+#define right A1
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+void setup() {
+  pinMode(left,INPUT);
+  pinMode(right,INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  Serial.println(digitalRead(left));
+  Serial.println(digitalRead(right));
+
+  if(digitalRead(left)==0 && digitalRead(right)==0){
+    //Forward
+    motor1.run(FORWARD);
+    motor1.setSpeed(150);
+    motor2.run(FORWARD);
+    motor2.setSpeed(150);
+    motor3.run(FORWARD);
+    motor3.setSpeed(150);
+    motor4.run(FORWARD);
+    motor4.setSpeed(150);
+  }
+  else if(digitalRead(left)==0 && !analogRead(right)==0){
+    //turn left
+    motor1.run(FORWARD);
+    motor1.setSpeed(200);
+    motor2.run(FORWARD);
+    motor2.setSpeed(200);
+    motor3.run(BACKWARD);
+    motor3.setSpeed(200);
+    motor4.run(BACKWARD);
+    motor4.setSpeed(200);
+    
+  }
+  else if(!digitalRead(left)==0 && digitalRead(right)==0){
+    //turn right
+    motor1.run(BACKWARD);
+    motor1.setSpeed(200);
+    motor2.run(BACKWARD);
+    motor2.setSpeed(200);
+    motor3.run(FORWARD);
+    motor3.setSpeed(200);
+    motor4.run(FORWARD);
+    motor4.setSpeed(200);
+   
+  }
+  else if(!digitalRead(left)==0 && !digitalRead(right)==0){
+    //stop
+    motor1.run(RELEASE);
+    motor1.setSpeed(0);
+    motor2.run(RELEASE);
+    motor2.setSpeed(0);
+    motor3.run(RELEASE);
+    motor3.setSpeed(0);
+    motor4.run(RELEASE);
+    motor4.setSpeed(0);
+   
+  }
+  
+}
